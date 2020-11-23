5a2b07a4baf17df33f2e67b62abcf225b606188a
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-11-23 11:55:25 +0000

    Update Structures.cpp
    added the data carrier

--- Structures.cpp
+++ Structures.cpp
@@ -1,7 +1,16 @@
 #include "Structures.h"
 
+
 template<typename T>
 struct TGraph
 {
 
+};
+
+struct DataCarrier
+{
+    std::string DefaultFontFileName;//Must end with ttf
+    std::strinig FilePath;
+
+    
 };
