e6e14c98891f02ff0c7205dffa43f14be694e9e7
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-11-23 09:28:54 +0000

    Update Structures.h

--- Structures.h
+++ Structures.h
@@ -2,6 +2,7 @@
 #include <vector>
 
 //Struct declarations
+//Yeah some of this is bad practise.
 
 struct Position
 {
@@ -18,3 +19,52 @@ struct Position
 		return pow((pow((x-Other.x),2)+pow((y-Other.y),2)),0.5);
 	}
 };
+
+
+template <typename T, typename X>
+bool Contains(X item, std::vector<T> v)
+{
+	return (std::find(v.begin(), v.end(), item) != v.end());
+	
+}
+
+template <typename T>
+int FindElem(T Elem, std::vector<T> Array)
+{
+	int idx = -1;
+	for (size_t i = 0; i < Array.size(); i++)
+	{
+		if (Array[i] == Elem)
+		{
+			idx = i;
+		}
+	}
+	return idx;
+}
+
+
+/*
+template <typename T>
+struct Atlas
+{
+	int size;
+	T* elements;
+};
+
+template <typename T>
+struct Node
+{
+	T value;
+	Node* Right;
+	Node* Left;
+
+};
+
+template <typename T>
+struct GraphNode
+{
+	T value;
+	TArray<GraphNode*> Connections;
+
+};
+*/
