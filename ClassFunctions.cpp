98e91a220f6a96da1f1233183ddd60349818867a
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-11-23 09:27:24 +0000

    Update ClassFunctions.cpp

--- ClassFunctions.cpp
+++ ClassFunctions.cpp
@@ -1,11 +1,16 @@
 #include "Creature.h"
 #include "Structures.h"
 #include "stdlib.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//Might be a lot better if I don't use classes for muscles and Bones
+//Or maybe just make muscle and bone be the same class with a different parameters
 
 //This function is used for creating a new random creature, it takes a seed if I want to use seeds.
 
 //std::vector 
-
 template<typename T>
 std::vector<T> AppendVector(std::vector<T> a, std::vector<T> b)
 {
@@ -33,7 +38,7 @@ Creature* GenerateNewRandom(int seed)
 	for (size_t i = 0; i < rand() % ConnectionLimit; i++)
 	{
 		//For random generation the connections need to be random
-		if (rand()%1 == 0)
+		if (rand() % 1 == 0)
 		{
 			Output->Muscles.push_back(new Muscle);
 		}
@@ -50,9 +55,9 @@ Creature* GenerateNewRandom(int seed)
 		//This is used to make sure that the nodes are atleast a small distance from each other
 		do
 		{
-			Output->Nodes[i]->Location = Position(rand() % 40 - 20,rand()% 40 -20);
+			Output->Nodes[i]->Location = Position(rand() % 40 - 20, rand() % 40 - 20);
 			//Remember all locations are relative to the location of the creature
-			
+
 
 			//TODO: make sure the nodes don't spawn to near to each other
 			break;
@@ -60,12 +65,12 @@ Creature* GenerateNewRandom(int seed)
 	}
 
 	//Now to use the connections to connect the nodes
-	
+
 	std::vector<Connection*> TempConnections;
 	TempConnections.insert(std::end(TempConnections), std::begin(Output->Bones), std::end(Output->Bones));
 	TempConnections.insert(std::end(TempConnections), std::begin(Output->Muscles), std::end(Output->Muscles));
 
-	int RandomNodeIdx1,RandomNodeIdx2;
+	int RandomNodeIdx1, RandomNodeIdx2;
 	for (size_t i = 0; i < TempConnections.size(); i++)
 	{
 		do
@@ -73,7 +78,7 @@ Creature* GenerateNewRandom(int seed)
 			//Make sure the random nodes are not the same
 			RandomNodeIdx1 = rand() % Output->Nodes.size();
 			RandomNodeIdx2 = rand() % Output->Nodes.size();
-			if (RandomNodeIdx1!=RandomNodeIdx2)
+			if (RandomNodeIdx1 != RandomNodeIdx2)
 			{
 				break;
 			}
@@ -88,7 +93,7 @@ Creature* GenerateNewRandom(int seed)
 	return Output;
 }
 
-Creature* CreateChild(Creature* Base,int seed)
+Creature* CreateChild(Creature* Base, int seed)
 {
 	//Creates another creature based on a creature, also takes a seed
 	Creature* Output = new Creature(*Base);
@@ -96,4 +101,215 @@ Creature* CreateChild(Creature* Base,int seed)
 	srand(seed);
 	//Now to randomise a few of the variables.
 
+	//The number of nodes can be increased or decreased at random by 1
+	if (rand() % 32 == 0)
+	{
+		//The chance that a node is either added or removed
+		if (rand() % 1 == 0)
+		{
+			Output->Nodes.push_back(new Node);
+			//Add new Node
+		}
+		else
+		{
+			Output->Nodes.pop_back();
+		}
+	}
+	int RandomNode;
+	for (size_t i = 0; i < Output->Nodes.size(); i++)
+	{
+		//firstly check to see if any node has no connections
+		if (Output->Nodes[i]->Connections.size() == 0)
+		{
+			//For all nodes that are found to be lacking a connection one will be generated to a random node.
+			//Randomise whether it will be a muscle or a bone
+			if (rand() % 1 == 0)
+			{
+				Output->Nodes[i]->Connections.push_back(new Muscle);
+				Output->Muscles.push_back((Muscle*)(Output->Nodes[i]->Connections[0]));
+				//Also needs to be added to the bones or muscles list
+			}
+			else
+			{
+				Output->Nodes[i]->Connections.push_back(new Bone);
+				Output->Bones.push_back((Bone*)Output->Nodes[i]->Connections[0]);
+			}
+			//Now to connect it to a random Node, can't be itself
+			do
+			{
+				RandomNode = rand() % Output->Nodes.size();
+
+			} while (RandomNode != i);
+				//Now add the connection to that Nodes
+				Output->Nodes[RandomNode]->Connections.push_back(Output->Nodes[i]->Connections[0]);
+		}
+
+	}
+
+	//Now the probability that the a bone will change to a muscle and a muscle to a bone
+
+	for (int i = 0; i < Output->Muscles.size(); i++)
+	{
+		if (rand() % 25 == 0)//of course all these random numbers might be changed
+		{
+			//Generate the replacement
+			Output->Bones.push_back(new Bone);
+			for (int j = 0; j < Output->Nodes.size(); j++)
+			{
+				
+				//if (Output->Nodes[j]->Connections.Contains(Output->Muscles[i]))
+				if(Contains(Output->Muscles[i], Output->Nodes[j]->Connections))
+				{
+					//Output->Nodes[j]->Connections[Output->Nodes[j]->Connections.find(Output->Muscles[i])] = Output->Bones[Output->Bones.size()];
+					Output->Nodes[j]->Connections[FindElem((Connection*)Output->Muscles[i], Output->Nodes[j]->Connections)] = Output->Bones[Output->Bones.size()];
+					
+						//That's a long and confusing line, it replaces references
+
+				}
+			}
+			Output->Muscles.erase(Output->Muscles.begin()+i);
+
+		}
+	}
+
+	for (int i = 0; i < Output->Bones.size(); i++)
+	{
+		//This is just copy and paste code from the muscle replacement
+		if (rand() % 25 == 0)
+		{
+
+			//Generate the replacement
+			Output->Muscles.push_back(new Muscle);
+			for (int j = 0; j < Output->Nodes.size(); j++)
+			{
+				//if (Output->Nodes[j]->Connections.Contains(Output->Bones[i]), Output->Nodes[j]->Connections)
+				if (Contains(Output->Bones[i], Output->Nodes[j]->Connections))
+				{
+					//Output->Nodes[j]->Connections[Output->Nodes[j].Connections.find(Output->Bones[i])] = Output->Muscles[Output.Muscles.size()];
+					Output->Nodes[j]->Connections[FindElem((Connection*)Output->Bones[i], Output->Nodes[j]->Connections)] = Output->Bones[Output->Muscles.size()];
+						//That's a long and confusing line, it replaces references
+
+				}
+			}
+			Output->Bones.erase(Output->Bones.begin()+i);
+		}
+
+
+	}
+
+	//Now to modify some of the values for the muscles, all bones are the same.
+	for (int i = 0; i < Output->Muscles.size(); i++)
+	{
+		if (rand() % 20 == 0)
+		{
+			//Change the strength of the Muscles
+			if (rand() % 1 == 0)
+			{
+				Output->Muscles[i]->Strength = Output->Muscles[i]->Strength * 1.01;
+			}
+			else
+			{
+				Output->Muscles[i]->Strength = Output->Muscles[i]->Strength * 0.99;
+			}
+		}
+
+		if (rand() % 25 == 0)
+		{
+			if (rand() % 1 == 0)
+			{
+				Output->Muscles[i]->Frequency = Output->Muscles[i]->Frequency * 1.01;
+			}
+			else
+			{
+				Output->Muscles[i]->Frequency = Output->Muscles[i]->Frequency * 0.99;
+			}
+		}
+	}
+
+	//Also want to modify the locations of the Nodes
+	for (int i = 0; i < Output->Nodes.size(); i++)
+	{
+		if (rand() % 25 == 0)
+		{
+			Output->Nodes[i]->Location.x += rand() % 4 - 2;
+			Output->Nodes[i]->Location.y += rand() % 4 - 2;
+		}
+	}
+
+	return Output;
 }
+
+std::vector<Creature*> Reproduce(std::vector<Creature*> StartCreatures, std::vector<float> Fitness, int seed)
+{
+	//The two lists need to be the same length
+	//Firstly need to order then according to fitness, use a pretty normal sorting algorithm list will only be a max 100 long
+	std::vector<Creature*> SortedCreatures;
+	float value;
+	int index;
+	for (int i = 0; i < Fitness.size(); i++)
+	{
+		value = Fitness[i];
+		index = 0;
+		for (int j = 0; j < Fitness.size(); j++)
+		{
+			if (Fitness[j] > Fitness[i])
+			{
+				value = Fitness[j];
+				index = j;
+			}
+		}
+		Fitness.erase(Fitness.begin()+i);
+		SortedCreatures.push_back(StartCreatures[i]);
+		StartCreatures.erase(StartCreatures.begin()+i);
+
+	}
+
+	//Now the half of the list with lower fitness
+	for (int i = 0; i < SortedCreatures.size() / 2; i++)
+	{
+		SortedCreatures.pop_back();
+	}
+
+
+	for (int i = 0; i < SortedCreatures.size(); i++)
+	{
+		SortedCreatures.push_back(CreateChild(SortedCreatures[i], seed));
+		seed++;
+		//Increment the seed so that the new creatures don't all have the same mutations
+	}
+
+	return SortedCreatures;
+
+}
+
+
+//Don't implement this until later
+/*
+void SaveCreatures(std::vector<Creature*> Creatures, std::string Filename)
+{
+	std::ofstream SaveFile;
+	SaveFile.open(Filename);
+
+	SaveFile << Creatures.size();
+	for (int i = 0; i < Creatures.size(); i++)
+	{
+		SaveFile << Creatures[i];
+	}
+
+	SaveFile.close();
+}
+
+std::vector<Creature*> LoadCreatures(std::string Filename)
+{
+	std::vector<Creature*> output;
+	std::ofstream SaveFile;
+	SaveFile.open(Filename);
+	int VectorSize;
+
+	//std::vector<Creature*> Output = new Creature[]
+
+
+	//Right now doesn't work
+	return output;
+}
+*/
