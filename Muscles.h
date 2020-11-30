7611204a62de6f59133f058bfbdc55df112a5f7c
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-11-30 11:59:23 +0000

    Update Muscles.h

--- Muscles.h
+++ Muscles.h
@@ -6,9 +6,22 @@ class Muscle :
 public:
 	float Frequency; //Really means how many seconds between each movement
 	float Strength;
+    bool MovingIn = false;
+    bool MovingOut = false;
+    
 
 	Muscle()
 	{
 
 	}
+	
+	void MuscleTick(float DeltaTime);
+
+
+private:
+    float MovementProgress = 1;
+    // ^No idea what this value should initially be
+	
+	
+	
 };
