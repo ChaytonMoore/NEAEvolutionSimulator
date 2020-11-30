cf1736eddded3b94a774fea58b7fa99fbe729843
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-11-30 11:57:52 +0000

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -12,17 +12,10 @@ void Node::WeightFunction()
 void Node::MuscleMovement()
 {
     Position DeltaPosition = Position(0,0);
-    Position RelativeMusclePosition;
-    for (int i = 0; i < Connections.size(); i++) 
-    {
-        RelativeMusclePosition = Connections[i].Location - Position;
-        if(Connections[i].Pulling)
-        {
-            //When it is pulling move that way by strength
-            DeltaPosition += RelativeMusclePosition * ((Muscle*)Connections[0]).strength;
-        }
-        
-    }
+    //This will find all the movements of all the nodes 
+    
+    
+    
     //Now move the node by what it should be moved by
     Position += DeltaPosition;
 }
