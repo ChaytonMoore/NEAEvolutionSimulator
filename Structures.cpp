256d57193ac219a9d84a996f27f6dbaf9c941ea5
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-11-30 09:43:06 +0000

    Update Structures.cpp

--- Structures.cpp
+++ Structures.cpp
@@ -1,4 +1,10 @@
 #include "Structures.h"
+#include <iostream>
+
+
+//So yes I deleted the stuff I'd done but it was shit
+//Remeber deletes some of the comments
+
 
 
 template<typename T>
@@ -7,10 +13,27 @@ struct TGraph
 
 };
 
-struct DataCarrier
+//I spent way too long debugging an error to do with this code only to discover it was todo with linkers.
+sf::Font GetDefaultFont(DataCarrier* GetFontFrom)
 {
-    std::string DefaultFontFileName;//Must end with ttf
-    std::strinig FilePath;
 
-    
-};
+	sf::Font font;
+	//if(!font.loadFromFile(GetFontFrom->FilePath + GetFontFrom->DefaultFontFileName))
+	if (!font.loadFromFile(GetFontFrom->getPath() + GetFontFrom->DefaultFontFileName))
+	{
+		std::cout << "error loading font";
+		return font;
+	}
+	std::cout << "font loaded";
+	
+	return font;
+}
+
+
+
+
+
+
+
+//remeber to actually use the TArray for the Node struct
+
