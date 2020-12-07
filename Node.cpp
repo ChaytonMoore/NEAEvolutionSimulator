baef1e64aadf9c21ebea54ff61e9a0aed96f7ea4
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-12-07 07:59:51 +0000

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,21 +1,116 @@
 #include "Node.h"
+#include "Muscle.h"
+#include <iostream>
 
 
+Node::Node()
+{
+
+
+}
 //Sink function
 void Node::WeightFunction()
 {
-    //Will move the node proportional to its weight every tick
-    
-    Location.y =- weight;
+	//Will move the node proportional to its weight every tick
+
+	Location.y = -Weight;
 }
 
 void Node::MuscleMovement()
 {
-    Position DeltaPosition = Position(0,0);
-    //This will find all the movements of all the nodes 
-    
-    
-    
-    //Now move the node by what it should be moved by
-    Position += DeltaPosition;
+	Position DeltaPosition = Position(0, 0);
+	//This will find all the movements of all the nodes 
+
+
+
+	//Now move the node by what it should be moved by
+	Location += DeltaPosition;
+
+	
+}
+
+
+//This should be applied to each node.
+Position Node::MovementResistance(float area, Position velocity)
+{
+	//www.khanacademy.org/computing/computer-programming/programming-natural-simulations/programming-forces/a/air-and-fluid-resistance
+	//^ That is where the function calcualtion comes from
+
+	//This whole thing is relative force
+
+	const float MovementConstant = 5;
+	//^This should be changed if stuff needs to be different
+	//float Output = -0.5 * area * pow(velocity,2) * MovementConstant;
+	Position Output = Position::NormaliseVector(velocity);
+	float CoefOutput;
+	CoefOutput = -0.5 * area * pow(Position::ModVector(velocity), 2) * MovementConstant;
+	Output.x = Output.x * CoefOutput;
+	Output.y = Output.y * CoefOutput;
+
+	
+	return Output;
+
+}
+
+Position Node::NodeVelocity(Node* InputNode)
+{
+	//Firstly get all the Muscles and their forces
+	//This is at school can't remeber where the stuff is will assume I've got a list of forces and a list of relative normalised positions
+
+	
+	std::vector<float> MuscleForces;
+	std::vector<Position> RelativePositions; //These lists need to be the same length these positions must also be normalised
+	Position Ta  = Position(0,0);
+	for (size_t i = 0; i < InputNode->Connections.size(); i++)
+	{
+		
+		if ((Muscle*)(InputNode->Connections[i]))
+		{
+			MuscleForces.push_back(((Muscle*)(InputNode->Connections[i]))->MuscleOutput);
+			RelativePositions.push_back(Position::NormaliseVector(InputNode->Location - ((Muscle*)(InputNode->Connections[i]))->Location));
+			//^ this line is a little too long to be nice
+
+			Ta = InputNode->Location - ((Muscle*)(InputNode->Connections[i]))->Location;
+			
+		}
+		
+		
+	}
+
+	
+
+
+	Position TotalForceDirection = Position(0, 0);
+	for (int i = 0; i < MuscleForces.size(); i++)
+	{
+		TotalForceDirection += RelativePositions[i] * MuscleForces[i];
+	}
+
+	if (TotalForceDirection.x > 0)
+	{
+		//std::cout << "force A " << TotalForceDirection.x << TotalForceDirection.y << std::endl;
+	}
+	
+
+	//Now the that the force is known the mass(weight) must be used to divide it to get the velocity;
+	TotalForceDirection += InputNode->MovementResistance(Area, LatentVelocity); //(TotalForceDirection + Node::MovementResistance(NodeOwner->Area, TotalForceDirection));
+
+	return TotalForceDirection / InputNode->Weight;
+
+}
+
+
+void Node::EventTick(float DeltaTime)
+{
+	
+	//Firstly need to move the node 
+	
+	LatentVelocity += NodeVelocity(this) * DeltaTime;;
+	Location += LatentVelocity;
+	if (abs(NodeVelocity(this).x) > 0)
+	{
+	//std::cout << "velocity " << NodeVelocity(this).x * DeltaTime<< NodeVelocity(this).y * DeltaTime << std::endl;
+	}
+	
+	
 }
