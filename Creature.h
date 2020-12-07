ad8c813e488ae31d59fd07e22ff67fc14694c307
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-12-07 08:00:07 +0000

    Update Creature.h

--- Creature.h
+++ Creature.h
@@ -9,15 +9,25 @@ public:
 	std::vector<Bone*> Bones;
 	std::vector<Muscle*> Muscles;
 	std::vector<Node*> Nodes;
+	float Area;
+	float Velocity;
+
 
 	void EventTick(float DeltaTime);
-	
+
+	void MuscleMovement();
+	Position GetAverageLocation();
+
 private:
-    //This variable is for the internal time of the creature starting from a zero point.
-    float LocalTimeValue = 0;
-    const float mFrequencyTolerance = 0.1;
-    //^ Used in the muscle movement if statement, might want to try different values.
-    
-    float Area;
+	//This variable is for the internal time of the creature starting from a zero point.
+	float LocalTimeValue = 0;
+	const float mFrequencyTolerance = 0.1;
+	//^ Used in the muscle movement if statement, might want to try different values.
+
+	void SetCreatureArea();
+
+	Position GetCreatureLocation();
+
+	Position OldLocation = Position(0,0);
 
 };
