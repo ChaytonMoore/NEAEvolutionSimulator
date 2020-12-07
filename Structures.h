28d08df15104874b0b2f77df445053f7c34b387f
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-12-07 08:01:33 +0000

    Update Structures.h

--- Structures.h
+++ Structures.h
@@ -1,20 +1,6 @@
-#pragma once
-#include <vector>
-#include <SFML/Graphics.hpp>
-#include <windows.h>
-
-//Struct declarations
-
-//bool operator>(const sf::Vector2i& a, const sf::Vector2i& b);
-
-
-//bool operator<(const sf::Vector2i& a, const sf::Vector2i& b);
-
-
-
 struct Position
 {
-	float x, y;
+	double x, y;
 
 	Position(float a, float b)
 	{
@@ -26,6 +12,54 @@ struct Position
 	{
 		return pow((pow((x-Other.x),2)+pow((y-Other.y),2)),0.5);
 	}
+
+	void operator +(const Position a)
+	{
+		this->x += a.x;
+		this->y += a.y;
+	}
+
+	void operator +=(const Position a)
+	{
+		this->x += a.x;
+		this->y += a.y;
+	}
+
+	static float ModVector(Position Input)
+	{
+		
+		return pow(pow(Input.x, 2) + pow(Input.y, 2), 0.5);
+	}
+
+	//This should be in the position strucuture
+	static Position NormaliseVector(Position Input)
+	{
+		//Probably something that does this in a library
+		if (ModVector(Input) != 0)
+		{
+			return Position(Input.x / ModVector(Input), Input.y / ModVector(Input));
+		}
+		else
+		{
+			return Position(0, 0);
+		}
+	}
+
+	Position operator *(float a)
+	{
+		return Position(this->x * a,this->y*a);
+	}
+
+	Position operator /(float a)
+	{
+		return Position(this->x / a, this->y / a);
+	}
+
+	Position operator -(Position a)
+	{
+		return Position(this->x - a.x, this->y - a.y);
+	}
+
 };
 
 struct DataCarrier
