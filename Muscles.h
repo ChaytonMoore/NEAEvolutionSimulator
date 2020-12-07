c667652b0412c2716c73f1596dbbce7eb4507ee5
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2020-12-07 08:03:36 +0000

    Update Muscles.h

--- Muscles.h
+++ Muscles.h
@@ -1,27 +1,31 @@
 #pragma once
 #include "Connection.h"
+#include "Node.h"
 class Muscle :
 	public Connection
 {
 public:
-	float Frequency; //Really means how many seconds between each movement
-	float Strength;
-    bool MovingIn = false;
-    bool MovingOut = false;
-    
+	float Frequency = 3; //Really means how many seconds between each movement
+	float Strength = 1;
+	bool MovingIn = false;
+	bool MovingOut = false;
+
 
 	Muscle()
 	{
 
 	}
-	
+
 	void MuscleTick(float DeltaTime);
 
+	float MuscleOutput = 0;
+
+	Node* ends[2];
 
 private:
-    float MovementProgress = 1;
-    // ^No idea what this value should initially be
-	
-	
-	
+	float MovementProgress = 1;
+	// ^No idea what this value should initially be
+
+
+
 };
