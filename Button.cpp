3e933609470f060a6baef50bdb8c145be9e4b301
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-02-26 23:13:23 +0000

    Update Button.cpp

--- Button.cpp
+++ Button.cpp
@@ -23,69 +23,106 @@ Button::Button(std::string ButtonText, DataCarrier* FontData, sf::Vector2f Butto
 	shape.setSize(ButtonSize);
 	shape.setOutlineColor(sf::Color::Red);
 	shape.setFillColor(sf::Color::Blue);
-	shape.setOutlineThickness(5);
+	shape.setOutlineThickness(1);
 	shape.setPosition(ButtonPosition);
 
-	BLeft = ButtonPosition;
-	TRight = ButtonPosition + ButtonSize;
-	
+	TLeft = ButtonPosition;
+	BRight = ButtonPosition + ButtonSize;
+
 
 }
 
 void Button::InitButton(std::string ButtonText, DataCarrier* FontData)
 {
-		// select the font
-		text.setFont(GetDefaultFont(FontData)); //Need to get the DataCarrier that contains the default font
+	// select the font
+	text.setFont(GetDefaultFont(FontData)); //Need to get the DataCarrier that contains the default font
 
-		// set the string to display
-		text.setString(ButtonText);
+	// set the string to display
+	text.setString(ButtonText);
 
-		// set the character size
-		text.setCharacterSize(12);
+	// set the character size
+	text.setCharacterSize(12);
 
-		// set up the button stuff
-		shape.setSize(sf::Vector2f(100, 50));
-		shape.setOutlineColor(sf::Color::Red);
-		shape.setOutlineThickness(5);
-		shape.setPosition(10, 20);
+	// set up the button stuff
+	shape.setSize(sf::Vector2f(100, 50));
+	shape.setOutlineColor(sf::Color::Red);
+	shape.setOutlineThickness(1);
+	shape.setPosition(10, 20);
 }
 
-	//This function is for when I don't want to put a button there.
+//This function is for when I don't want to put a button there.
 void Button::InitButton(DataCarrier* FontData)
 {
-		// select the font
-		text.setFont(GetDefaultFont(FontData)); //Need to get the DataCarrier that contains the default font
+	// select the font
+	text.setFont(GetDefaultFont(FontData)); //Need to get the DataCarrier that contains the default font
 
-		// set the string to display
-		text.setString("Button");
+	// set the string to display
+	text.setString("Button");
 
-		// set the character size
-		text.setCharacterSize(12);
+	// set the character size
+	text.setCharacterSize(12);
 }
 
 
-bool Button::isClicked()
+bool Button::isClicked(sf::RenderWindow* Window)
 {
-		sf::Vector2i MousePosition = sf::Mouse::getPosition();;
-		if (MousePosition.x > BLeft.x && MousePosition.y > BLeft.y && MousePosition.x < TRight.x && MousePosition.y < TRight.y && sf::Mouse::isButtonPressed(sf::Mouse::Left))
-		{
-			isPressed = true;
-		}
-		else
-		{
-			isPressed = false;
-		}
-		return isPressed;
-}
 
-void Button::ButtonTick()
-{
-		//This is the code for the button that is executed every tick
-	if (DefaultFunctionality&&isClicked())
+
+	if (ButtonUsable)
 	{
-		fcnPtr();
-		std::cout << "clicked" << std::endl;
+		
+
+		ButtonUsable = false;
+
+		return MouseInButton(Window) && sf::Mouse::isButtonPressed(sf::Mouse::Left);
+	}
+	else
+	{
+		return false;
+	}
+	
+}
+
+
+bool Button::MouseInButton(sf::RenderWindow* Window)
+{
+	return ((Window->getPosition().x + int(TLeft.x) < sf::Mouse::getPosition().x) && (Window->getPosition().y + int(TLeft.y) < sf::Mouse::getPosition().y - 30) && (Window->getPosition().x + int(BRight.x) > sf::Mouse::getPosition().x) && (Window->getPosition().y + int(BRight.y) > sf::Mouse::getPosition().y-30));
+}
+
+void Button::ButtonTick(sf::RenderWindow* Window)
+{
+	//This is the code for the button that is executed every tick
+	if (DefaultFunctionality&&MouseInButton(Window)&& sf::Mouse::isButtonPressed(sf::Mouse::Left)&&ButtonUsable)
+	{
+		fcnPtr(Window);
+		//std::cout << "clicked" << std::endl;
+		ButtonUsable = false;
+		
 		//Executes the buttons default code
 	}
-	std::cout << sf::Mouse::getPosition().x << std::endl;
+	if (MouseInButton(Window))
+	{
+		isPressed = true;
+	}
+	else
+	{
+		isPressed = false;
+	}
+
+	if (!ButtonUsable)
+	{
+
+		if (!sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			
+			ButtonUsable = true;
+		}
+	}
+	
+	//else
+	//{
+	//	std::cout << Window->getPosition().y + int(TLeft.y) <<""<<sf::Mouse::getPosition().y<<std::endl;
+	//}
+
+	//std::cout << sf::Mouse::getPosition().x << std::endl;
 }
